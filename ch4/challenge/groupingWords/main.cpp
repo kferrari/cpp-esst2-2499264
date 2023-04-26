85e340d0ca6205b22e8c67fd0904c44556748543
Kim <kim.ferrari@vigilitech.ch>
2023-04-26 09:39:25 +0200

    cha4

--- ch4/challenge/groupingWords/main.cpp
+++ ch4/challenge/groupingWords/main.cpp
@@ -12,7 +12,33 @@ int main()
     std::vector<std::string> strs = {"eat","tea","tan","ate","nat","bat"};
     // Ergebnis soll ungefähr so aussehen: { {"bat"}, {"nat","tan"}, {"ate","eat","tea"} }
 
-    /*
+    std::map< std::string, std::vector< std::string > > groups;
+
+    // define lambda to convert letters to key
+    auto wordToKey = [](const std::string &word) -> std::string {
+        // add letters to (ordered) set
+        std::set<char> temp;
+
+        for (const auto &c : word){
+            temp.insert(c);
+        }
+
+        // convert back to string
+        std::string key;
+        for (const auto &letter : temp){
+            key += letter;
+        }
+
+        return key;
+    };
+
+    for( const auto &str : strs )
+    {
+        const auto key = wordToKey(str);
+        groups[key].push_back(str);
+    }
+
+
     for( const auto &group : groups )
     {
         std::cout << "Group " << group.first << ":" << std::endl;
@@ -22,6 +48,6 @@ int main()
         }
         std::cout << std::endl;
     }
-*/
+
     return 0;
 }
